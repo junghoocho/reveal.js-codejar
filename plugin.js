dacad142276c6cbb97786920da9f7a2cef689ac8
Junghoo Cho <cho@cs.ucla.edu>
2020-06-14 08:43:39 -0700

    RevealCodeJar now has codejar() method to let others create CodeJar editor

--- plugin.js
+++ plugin.js
@@ -12,32 +12,45 @@ const pluginID = 'codejar';
 let options = {};
 
 let deck = null;
-let hljs = null;
 
+// text highlight function
 function highlight(editor) {
-    // highlight.js does not trim old tags, let's do it by this hack.
-    editor.textContent = editor.textContent;
-    if (hljs) hljs.highlightBlock(editor);
+    // get highlight plugin
+    let hljs = deck.getPlugin('highlight');
+
+    if (hljs) {
+        // highlight.js does not trim old tags, let's do it by this hack.
+        editor.textContent = editor.textContent;
+        hljs.hljs.highlightBlock(editor)
+    }
 }
 
+// create editor from the passed element with the given options
+function createEditor(element) {
+    let highlighter = highlight;
+
+    // check if line numbers are needed
+    if (element.hasAttribute("data-line-numbers") &&
+        element.getAttribute("data-line-numbers").toLowerCase() !== "false") {
+        highlighter = withLineNumbers(highlight);
+    }
+
+    // stop keypress event propagating upward
+    element.addEventListener("keypress", (event) => { event.stopPropagation(); });
+
+    // create CodeJar Editor
+    return CodeJar(element, highlighter, options);
+}
+
+// setup CodeJar editors
 function setupEditors() {
     if (!deck.isReady()) {
         deck.on('ready', setupEditors);
         return;
     }
 
-    // get highlight.js
-    if (deck.getPlugin('highlight')) {
-        hljs = deck.getPlugin('highlight').hljs;
-    }
-
-    for (let e of deck.getRevealElement().querySelectorAll('.codejar')) {
-        let highlighter = highlight;
-        if (e.hasAttribute("data-line-numbers") && e.getAttribute("data-line-numbers").toLowerCase() !== "false") {
-            highlighter = withLineNumbers(highlight);
-        }
-        e.addEventListener("keypress", (event) => { event.stopPropagation(); });
-        CodeJar(e, highlighter, options);
+    for (let editor of deck.getRevealElement().querySelectorAll('.codejar')) {
+        createEditor(editor, options);
     }
 }
 
@@ -54,11 +67,11 @@ function init(reveal) {
     let head = document.getElementsByTagName('head')[0];
     let style = document.createElement('style');
     style.type = "text/css";
-    style.innerHTML = `.codejar { border-radius: 6px; box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2); font-family: "Source Code Pro", monospace; font-size: 0.9em; line-height: 1.2em; padding: 10px; tab-size: 4; }`;
+    style.innerHTML = `.codejar { border-radius: 6px; box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2); font-family: "Source Code Pro", monospace; font-size: 0.85em; line-height: 1.15em; padding: 10px; tab-size: 4; }`;
     head.appendChild(style);
 
     // setup editors
     setupEditors();
 }
 
-window.RevealCodeJar = window.RevealCodeJar || { id: pluginID, init: init };
+window.RevealCodeJar = window.RevealCodeJar || { id: pluginID, init: init, codejar: createEditor };
